2198779296489d7cdcba1c57daaacdb44e3f0818
Hoang Nguyen <infamousingamine@gmail.com>
2022-04-06 23:36:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <èdsfdsfds
+        <img src={logo} className="App-logo" alt="logo"/>
       </header>
     </div>
   );
