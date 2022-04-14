d3f72057c929a5e2fc9b5c932ac6b1eabc652de7
Hoang Nguyen <infamousingamine@gmail.com>
2022-04-14 15:17:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const age = 18;
+  const isFemale = true;
+  const student = {
+    name: 'asy Frontend',
+  };
+  const colorList = ['red', 'green', 'blue'];
   return (
     <div className="App">
       <header className="App-header">
